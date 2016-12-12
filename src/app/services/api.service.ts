a0c5762e8cd7de05cd5410cdf2c4cb82edfac4f5
AcarMeel <acarmeel@gmail.com>
2016-12-11 18:33:16 -0600

    services

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 
@@ -8,4 +11,32 @@ export class ApiService {
         'Content-type': 'application/json',
         Accept: 'application/json'
     });
+
+    api_url : string = 'http://localhost:3000';
+
+    constructor(private http: Http) {
+        
+    }
+
+    private getJson(res : Response) {
+        return res.json();
+    }
+
+    private checkForError(resp: Response): Response {
+        if(resp.status >= 200 && resp.status < 300) {
+            return resp;
+        } else {
+            const error = new Error(resp.statusText);
+            error['response'] = resp;
+            console.log(error);
+            throw error;
+        }
+    }
+
+    get(path: string): Observable<any> {
+        return this.http.get(`${this.api_url}${path}`, this.headers)
+        .map(this.checkForError)
+        .catch(err => Observable.throw(err))
+        .map(this.getJson)
+    }
 }
